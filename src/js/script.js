f60ff8f2bc1eeb98c13be223fd046e9684b297f8
Andrey <fandrey736@gmail.com>
2020-03-05 13:51:24 +0800

    Commit with carousel, but no adaptation

---
+++ src/js/script.js
@@ -0,0 +1,52 @@
+// if (){
+
+// }else if(){
+
+// }else{
+
+// }
+
+// for (let i=1;i<8;i++){
+//     console.log(i);
+// }
+
+// function logging(){
+//     console.log("hello world")
+// }
+// logging();
+
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        infinite: true,
+        speed: 700,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        responsive:[{
+            breakpoint: 992,
+            settings:{
+                arrows: false
+            }
+        }]
+      });
+
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+    function toggleSlide(item){
+        $(item).each(function(i){
+            $(this).on('click', function(e){
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
+            })
+        })
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+});
